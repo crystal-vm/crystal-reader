e2054660ce2e5b571f557c358f1c3401c93e441d
Torsten Ruger <torsten@villataika.fi>
2015-09-19 14:53:59 +0300

    thought to be useful sat addition

--- lib/salama-reader.rb
+++ lib/salama-reader.rb
@@ -1,3 +1,22 @@
 require 'parslet'
 require 'parser/salama'
+require "ast"
 require 'parser/transform'
+
+AST::Node.class_eval do
+
+  def [](name)
+    puts self.inspect
+    children.each do |child|
+      if child.is_a?(AST::Node)
+        puts child.type
+        if (child.type == name)
+          return child.children
+        end
+      else
+        puts child.class
+      end
+    end
+    nil
+  end
+end
